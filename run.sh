7d87fb7e0f007e36cd9da44bb4190e838a8f3ae1
Jerome Gennevive <jerome.gennevieve@bayard-presse.com>
2020-03-29 18:57:54 +0200

    Modified version for docker Modified URLs for LX Added budget

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 # Instead use the latest tagged version as the next row
 # DOCKER_CONTAINER=sitespeedio/sitespeed.io:10.1.0
 
-DOCKER_CONTAINER=sitespeedio/sitespeed.io-autobuild:latest
+DOCKER_CONTAINER=sitespeedio/sitespeed.io-autobuild:12.3.1
 DOCKER_SETUP="--cap-add=NET_ADMIN  --shm-size=2g --rm -v /config:/config -v "$(pwd)":/sitespeed.io -v /etc/localtime:/etc/localtime:ro -e MAX_OLD_SPACE_SIZE=3072 "
 CONFIG="--config /sitespeed.io/config"
 BROWSERS=(chrome firefox)
@@ -24,16 +24,16 @@ for url in tests/$TEST/desktop/urls/*.txt ; do
     done
 done
 
-for script in tests/$TEST/desktop/scripts/*.js ; do
-    [ -e "$script" ] || continue
-    for browser in "${BROWSERS[@]}"  ; do
-        POTENTIAL_CONFIG="./config/$(basename ${script%%.*}).json"
-        [[ -f "$POTENTIAL_CONFIG" ]] && CONFIG_FILE="$(basename ${script%.*}).json" || CONFIG_FILE="desktop.json"
-        NAMESPACE="--graphite.namespace sitespeed_io.$(basename ${script%%.*})"
-        docker run $DOCKER_SETUP $DOCKER_CONTAINER $NAMESPACE $CONFIG/$CONFIG_FILE --multi -b $browser --spa $script
-        control
-    done
-done
+# for script in tests/$TEST/desktop/scripts/*.js ; do
+#     [ -e "$script" ] || continue
+#     for browser in "${BROWSERS[@]}"  ; do
+#         POTENTIAL_CONFIG="./config/$(basename ${script%%.*}).json"
+#         [[ -f "$POTENTIAL_CONFIG" ]] && CONFIG_FILE="$(basename ${script%.*}).json" || CONFIG_FILE="desktop.json"
+#         NAMESPACE="--graphite.namespace sitespeed_io.$(basename ${script%%.*})"
+#         docker run $DOCKER_SETUP $DOCKER_CONTAINER $NAMESPACE $CONFIG/$CONFIG_FILE --multi -b $browser --spa $script
+#         control
+#     done
+# done
 
 for url in tests/$TEST/emulatedMobile/urls/*.txt ; do
     [ -e "$url" ] || continue
@@ -44,14 +44,14 @@ for url in tests/$TEST/emulatedMobile/urls/*.txt ; do
     control
 done
 
-for script in tests/$TEST/emulatedMobile/scripts/*.js ; do
-    [ -e "$script" ] || continue
-    POTENTIAL_CONFIG="./config/$(basename ${script%%.*}).json"
-    [[ -f "$POTENTIAL_CONFIG" ]] && CONFIG_FILE="$(basename ${script%.*}).json" || CONFIG_FILE="emulatedMobile.json"
-    NAMESPACE="--graphite.namespace sitespeed_io.$(basename ${script%%.*})"
-    docker run $DOCKER_SETUP $DOCKER_CONTAINER $NAMESPACE $CONFIG/$CONFIG_FILE --multi --spa $script
-    control
-done
+# for script in tests/$TEST/emulatedMobile/scripts/*.js ; do
+#     [ -e "$script" ] || continue
+#     POTENTIAL_CONFIG="./config/$(basename ${script%%.*}).json"
+#     [[ -f "$POTENTIAL_CONFIG" ]] && CONFIG_FILE="$(basename ${script%.*}).json" || CONFIG_FILE="emulatedMobile.json"
+#     NAMESPACE="--graphite.namespace sitespeed_io.$(basename ${script%%.*})"
+#     docker run $DOCKER_SETUP $DOCKER_CONTAINER $NAMESPACE $CONFIG/$CONFIG_FILE --multi --spa $script
+#     control
+# done
 
 # We run WebPageReplay just to verify that it works
 for url in tests/$TEST/replay/urls/*.txt ; do
@@ -72,12 +72,12 @@ for url in tests/$TEST/webpagetest/desktop/urls/*.txt ; do
 done
 
 # You can also test using WebPageTest scripts
-for script in tests/$TEST/webpagetest/desktop/scripts/* ; do
-    [ -e "$script" ] || continue
-    NAMESPACE="--graphite.namespace sitespeed_io.$(basename ${script%%.*})"
-    docker run $DOCKER_SETUP $DOCKER_CONTAINER $NAMESPACE $CONFIG/webpagetest.json --plugins.remove browsertime --webpagetest.file $script https://www.example.org/
-    control
-done
+# for script in tests/$TEST/webpagetest/desktop/scripts/* ; do
+#     [ -e "$script" ] || continue
+#     NAMESPACE="--graphite.namespace sitespeed_io.$(basename ${script%%.*})"
+#     docker run $DOCKER_SETUP $DOCKER_CONTAINER $NAMESPACE $CONFIG/webpagetest.json --plugins.remove browsertime --webpagetest.file $script https://www.example.org/
+#     control
+# done
 
 # Remove the current container so we fetch the latest autobuild the next time
 # If you run a stable version (as YOU should), you don't need to remove the container,
