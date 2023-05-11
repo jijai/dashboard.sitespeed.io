cb3e7308ac8bd873eac0e2efcba20fb19a7459a3
administrateur <administrateur@LIN-MON-LX-WEB-01.bayard.lan>
2023-05-11 16:53:56 +0200

    added tests with consent in the scheduler

--- runLC.sh
+++ runLC.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 
 # We use the autobuild to always test our new functionality. But YOU should not do that!
 # Instead use the latest tagged version as the next row
-DOCKER_CONTAINER=sitespeedio/sitespeed.io:26.1.0-plus1
+DOCKER_CONTAINER=sitespeedio/sitespeed.io:27.3.0-plus1
 
 DOCKER_SETUP="--cap-add=NET_ADMIN --network dashboardsitespeedio_backend --shm-size=4g --rm -v /config:/config -v "$(pwd)":/sitespeed.io -v /etc/localtime:/etc/localtime:ro -e MAX_OLD_SPACE_SIZE=3072 "
 DESKTOP_BROWSERS_DOCKER=(chrome firefox)
@@ -34,6 +34,12 @@ for file in testsLC/docker/emulatedMobile/*.{txt,js} ; do
         control
     done
 done
+
+docker run $DOCKER_SETUP $DOCKER_CONTAINER --config configLC/mobile_lacroix_article.json -b chrome https://www.la-croix.com/Quatre-morts-blesses-avalanche-Alpes-francaises-2023-04-09-1301262714
+docker run $DOCKER_SETUP $DOCKER_CONTAINER --config configLC/mobile_marianne_article.json -b chrome https://www.marianne.net/societe/laicite-et-religions/menaces-de-cathos-radicaux-la-justice-ouvre-une-enquete-apres-lannulation-du-concert-de-bilal-hassani
+docker run $DOCKER_SETUP $DOCKER_CONTAINER --config configLC/mobile_humanite_article.json -b chrome https://www.humanite.fr/politique/fabien-roussel-nous-voulons-etre-un-parti-pleinement-feministe-790186
+# docker run $DOCKER_SETUP $DOCKER_CONTAINER --config configLC/mobile_lequipe_article.json -b chrome https://www.lequipe.fr/Cyclisme-sur-route/Actualites/Mathieu-van-der-poel-remporte-paris-roubaix-en-solitaire/1390610
+#docker run $DOCKER_SETUP $DOCKER_CONTAINER --config configLC/mobile_lemonde_article.json -b chrome https://www.lemonde.fr/economie/article/2021/12/16/jean-pierre-farandou-pdg-de-la-sncf-sur-les-salaires-le-laisser-faire-d-aujourd-hui-nous-le-paierons-demain_6106284_3234.html 
 
 # Remove the current container so we fetch the latest autobuild the next time
 # If you run a stable version (as YOU should), you don't need to remove the container,
